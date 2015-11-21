b9c1c59eef2fcf64ba7c7ddf83d3815d0bcf91ab
erickfmm <erickfmm@gmail.com>
2015-11-21 00:25:16 -0300

    Cambiar consultas y refactorizar base de datos
    El error de refresco de las consultas, era porque no enmascaré la
    consulta en una función (mi error).
    Además separé la definición de la base de datos, de las asociaciones y
    el resto. de manera de poder usar el modulo sequelize por separado de
    db, en cualquier parte del código.
    Cambié el nombre del archivo "consultas" a "asignaturas", para que sea
    más representativo.
    Además, rabié un rato para hacer la consulta con join usando ORM, así
    que terminé de hacer esas con sql puro.

--- app/models/index.js
+++ app/models/index.js
@@ -3,40 +3,16 @@ var fs = require('fs'),
   path = require('path'),
   Sequelize = require('sequelize'),
   config = require('../../config/config'),
-  db = {};
+  db = {},
+  sequelize = require('./sequelize.js');
 
 /*Acá se define el objeto de la base de datos, donde los parámetros son:
 database, usuario, password, opciones
 */
-var sequelize = new Sequelize('teclerasuv', 'teclerasuv', 'desarrolloweb', {
-
-  host: 'localhost',
-  dialect: 'mysql',
-  autoIncrement: true,
-  pool: {
-    /*Estas son las conexiones máximas, mínimas, y el tiempo de espera máximo, si da error timeout, le suben el idle :v yo le subi el min para que espere que haya al menos una conexión
-    */
-    max: 50,
-    min: 1,
-    idle: 10
-  },
-  define: {
-    /*esto crea columnas de timestamp, es decir, cuándo se creó la fila, cuándo se actualizó por última vez. para desactivar eso, ponerlo en false
-    */
-        timestamps: false,
-        /*esto es debido a que sequelize  es muy estricto con que se debe tener una bd estandarizada, con los nombres de las tablas todos terminados en s, entonces si tus tablas no terminan todas en s, entonces pon esto
-        */
-        freezeTableName: true,
-        /*esto es para las columnas, a sequelize le gusta que todas estén en camelCase, o en under_score, no mezcladas, si las tienes mezcladas se hace un lío, no lo recomiendo... volás del ORM
-        */
-        underscored: true
-  }
-
-});
 /*esto crea el namespace común "db"
 */
 fs.readdirSync(__dirname).filter(function (file) {
-  return (file.indexOf('.') !== 0) && (file !== 'index.js');
+  return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file !== 'sequelize.js');
 }).forEach(function (file) {
   var model = sequelize['import'](path.join(__dirname, file));
   db[model.name] = model;
