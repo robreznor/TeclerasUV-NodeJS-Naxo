2eba1e8cd82962cc617ea1ca34afcfd1b993d593
jorgegomezv <jorgegverdejo@gmail.com>
2015-12-04 14:34:06 -0300

    Subir Imagen

--- app/controllers/docentecrearpreguntaalternativa.js
+++ app/controllers/docentecrearpreguntaalternativa.js
@@ -1,8 +1,10 @@
 var express = require('express'),
   router = express.Router(),
   auth_docente = require("../middleware/auth_docente.js"),
-  queries = require('../queries/index.js');
-
+  queries = require('../queries/index.js'),
+  path = require('path'),
+  fs = require('fs'),
+  multipart = require('connect-multiparty');
 module.exports = function(app) {
 
   var bodyParser = require('body-parser');
@@ -16,9 +18,12 @@ module.exports = function(app) {
     console.log("id usuario:",request.session.name, "tipo:", request.session.tipo);
     response.render('docentecrearpreguntaalternativa', {});
   });
-  router.post("crearpreguntaalternativa", auth_docente, function(request, response, next) {
+  router.post("/crearpreguntaalternativa",multipart(),  auth_docente, function(request, response, next) {
+    console.log(request.files);
+    console.log(request.body);
     queries.gestionar_pregunta.insertar_pregunta(request.body.nombrepregunta, request.body.pregunta , request.body.url_video , request.body.explicacion)
       .then(function(insertado_pregunta) {
+        
         console.log("insertado:", insertado_pregunta);
         response.redirect("/docente/crearpreguntaalternativa");
         return;
@@ -27,7 +32,8 @@ module.exports = function(app) {
         console.log(error);
         /*Como ven, acá hay dos redirect, pero no se ejecutan uno y después el otro, ya que cuando entra a un callback, el flujo de ejecución cambia
          */
-        response.redirect("crearpreguntaalternativa");
+        //response.redirect("crearpreguntaalternativa");
+        next();
       })
   })
 }
