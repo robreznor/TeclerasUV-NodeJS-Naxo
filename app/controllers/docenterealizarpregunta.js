c8a99fe231598fdce650662ce13bfed744fea3fa
jorgegomezv <jorgegverdejo@gmail.com>
2015-12-04 13:44:00 -0300

    Crear Pregunta Aleatoria

--- app/controllers/docenterealizarpregunta.js
+++ app/controllers/docenterealizarpregunta.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   router = express.Router(),
   auth_docente = require("../middleware/auth_docente.js"),
-  preguntas = require('../queries/preguntas.js');
+  queries = require('../queries/index.js');
 
 module.exports = function(app) {
 
@@ -12,26 +12,7 @@ module.exports = function(app) {
   }));
   app.use('/', router);
 
-  //router consulta las preguntas datos dela asignatura
-
   router.get('/docente/realizar/:idasignatura/:idparalelo', auth_docente, function(request, response, next) {
-
-    preguntas.consultas.buscar_preguntas_asignatura(request.params.idasignatura, request.params.idparalelo)
-    .then(function(preguntas_res) {
-      console.log("Preguntas del ramo/paralelo: ",preguntas_res)
-
-      var preguntas = [];
-      for(i in preguntas_res){
-        preguntas.push({
-          id: preguntas_res[i].PM_ID,
-          nombre: preguntas_res[i].PM_NOMBRE,
-          tipo: preguntas_res[i].PM_TIPO,
-        })
-      }
-
-
-    response.render('docenterealizarpregunta', {preguntas: preguntas});
-    })
+    response.render('docenterealizarpregunta', {});
   });
-
 }
--- app/queries/gestionar_pregunta.js
+++ app/queries/gestionar_pregunta.js
@@ -2,4 +2,14 @@ var db = require('../models');
 
 exports.consultas = {
 //TODO 
+	insertar_pregunta: function(nombrepregunta, pregunta , url_video , explicacion) {
+    return db.TV_PREGUNTA_MAESTRA
+      .build({
+        PM_NOMBRE: nombrepregunta,
+        PM_TEXTO: pregunta,
+        PM_RUTA_VIDEO : url_video,
+        PM_EXPLICACION : explicacion
+      })
+      .save()
+  }
 }
